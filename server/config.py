a2021af92eaba51c1bda5fbae64ef18d8b210408
Alex <alexander.clayton.email@gmail.com>
2024-03-11 21:44:55 -0500

    migrating to JWT for user authentication

--- server/config.py
+++ server/config.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 from datetime import timedelta
 import os
@@ -9,6 +10,7 @@ import os
 load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
 SECRET_KEY = os.getenv("SECRET_KEY")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 app = Flask(__name__)
 CORS(app)
@@ -16,8 +18,10 @@ CORS(app)
 bcrypt = Bcrypt(app)
 
 app.config["MONGO_URI"] = MONGODB_URI
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
 
 app.secret_key = SECRET_KEY
 
 mongo = PyMongo(app)
+jwt = JWTManager(app)
