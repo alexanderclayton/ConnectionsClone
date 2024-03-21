f03271e9bc7c7d2156f08c87c04e20fd847c58d2
Alex <alexander.clayton.email@gmail.com>
2024-03-20 22:10:59 -0500

    clear tokens, style login

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -18,48 +18,45 @@ export const Signin = () => {
   };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <h2 className="text-2xl font-bold">Log in or create an account</h2>
+    <div className="flex h-full w-full flex-col items-center justify-center p-3">
+      <h2 className="mt-4 text-3xl">Log in or create an account</h2>
       <form
         onSubmit={(e) => handleSubmit(e, username, password)}
-        className="flex flex-col"
+        className="flex w-full max-w-96 flex-col"
       >
-        <label htmlFor="username" className="text-sm font-bold">
-          Username:
+        <label htmlFor="username" className="mt-6 text-sm font-bold">
+          Username
         </label>
         <input
           type="text"
           id="username"
-          className="rounded-sm border"
+          className="mt-1 h-12 rounded-sm border border-black pl-4 focus:outline-none"
           onChange={(e) => setUsername(e.target.value)}
           value={username}
         />
-        <label htmlFor="password" className="text-sm font-bold">
-          Password:
+        <label htmlFor="password" className="mt-4 text-sm font-bold">
+          Password
         </label>
         <input
           type="text"
           id="password"
-          className="rounded-sm border"
+          className="mt-1 h-12 rounded-sm border border-black pl-4 focus:outline-none"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
         <input
           type="submit"
           value="Log In"
-          className="rounded-sm bg-black font-bold text-white hover:cursor-pointer"
+          className="mt-4 h-12 rounded-sm bg-black font-bold text-white hover:cursor-pointer"
         />
       </form>
       <button
-        className="rounded-sm bg-black font-bold text-white hover:cursor-pointer"
+        className="mt-4 h-12 w-full max-w-96 rounded-sm border border-black font-bold hover:cursor-pointer"
         onClick={() => navigate("/createaccount")}
       >
         Create Account
       </button>
-      <button
-        className="rounded-sm bg-black font-bold text-white hover:cursor-pointer"
-        onClick={() => navigate("/")}
-      >
+      <button className="mt-4 " onClick={() => navigate("/")}>
         Return Home
       </button>
     </div>
